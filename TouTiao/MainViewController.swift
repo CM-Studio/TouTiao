7fc862ede1105731794d8cf65b7e1f1b123c24ec
judi_pc <judi0713@foxmail.com>
2016-04-01 12:55:39 +0800

    新建popview

---
+++ TouTiao/MainViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MainViewController.swift
+//  TouTiao
+//
+//  Created by tesths on 4/1/16.
+//  Copyright © 2016 tesths. All rights reserved.
+//
+
+import Cocoa
+
+class MainViewController: NSViewController {
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
+    let popover = NSPopover()
+    
+    override func awakeFromNib() {
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = #selector(MainViewController.togglePopover(_:))
+        }
+        
+        popover.contentViewController = PopViewController(nibName: "PopViewController", bundle: nil)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        
+        
+        
+    }
+    
+    func togglePopover(sender: AnyObject?) {
+        if popover.shown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
+        }
+    }
+    
+    func showPopover(sender: AnyObject?) {
+        if let button = statusItem.button {
+            popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSRectEdge.MinY)
+        }
+    }
+    
+    func closePopover(sender: AnyObject?) {
+        popover.performClose(sender)
+    }
+}
---
+++ TouTiao/PopViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PopViewController.swift
+//  TouTiao
+//
+//  Created by tesths on 4/1/16.
+//  Copyright © 2016 tesths. All rights reserved.
+//
+
+import Cocoa
+
+class PopViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
