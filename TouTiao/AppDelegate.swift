d4d92e8c6df18f44e401e40affda41211fd5425a
judi_pc <judi0713@foxmail.com>
2016-04-01 12:14:00 +0800

    Initial Commit

---
+++ TouTiao/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TouTiao
+//
+//  Created by tesths on 4/1/16.
+//  Copyright © 2016 tesths. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
